a64f1336dd17b7cee18b110771b45baad2bc1b8d
Nyan Lin Tun <ethicalvixix@gmail.com>
2020-04-05 06:05:33 +0630

    Project Initializd with some UI components

---
+++ Meme/Extensions/UIImagePickerExtension.swift
@@ -0,0 +1,14 @@
+//
+//  UIImagePickerExtension.swift
+//  Meme
+//
+//  Created by Nyan Lin Tun on 05/04/2020.
+//  Copyright © 2020 Nyan Lin Tun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension MemeViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+}
---
+++ Meme/MemeViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Meme
+//
+//  Created by Nyan Lin Tun on 05/04/2020.
+//  Copyright © 2020 Nyan Lin Tun. All rights reserved.
+//
+
+import UIKit
+
+class MemeViewController: UIViewController {
+
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var memeImageView: UIImageView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    @IBAction func fontsAction(_ sender: UIBarButtonItem) {
+    
+    }
+    
+    
+    @IBAction func albumAction(_ sender: UIBarButtonItem) {
+        let photoPickerVC = UIImagePickerController()
+        photoPickerVC.delegate = self
+        photoPickerVC.sourceType = .photoLibrary
+//        photoVC.modalPresentationStyle = .fullScreen
+        self.present(photoPickerVC, animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraAction(_ sender: UIBarButtonItem) {
+        
+    }
+    
+}
+
