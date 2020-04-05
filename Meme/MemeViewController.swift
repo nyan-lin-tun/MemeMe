2100770c837c3bfdc79176500f31d3fa486b1d8e
Nyan Lin Tun <ethicalvixix@gmail.com>
2020-04-06 04:20:03 +0630

    Refactor ImagePicker and setUp textfields

--- Meme/MemeViewController.swift
+++ Meme/MemeViewController.swift
@@ -28,23 +28,20 @@ class MemeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setUpTextFields()
-
     }
     
     private func setUpTextFields() {
-        self.topTextField.delegate = self
-        self.bottomTextField.delegate = self
-        self.topTextField.defaultTextAttributes = TextAttributes.attribute
-        self.bottomTextField.defaultTextAttributes = TextAttributes.attribute
-        self.topTextField.text = "TOP"
-        self.bottomTextField.text = "BOTTOM"
-        self.topTextField.textAlignment = .center
-        self.bottomTextField.textAlignment = .center
-        self.topTextField.adjustsFontSizeToFitWidth = true
-        self.bottomTextField.adjustsFontSizeToFitWidth = true
+        self.setUpTextField(textField: self.topTextField, text: "TOP")
+        self.setUpTextField(textField: self.bottomTextField, text: "BOTTOM")
     }
     
-    
+    private func setUpTextField(textField: UITextField, text: String) {
+        textField.delegate = self
+        textField.defaultTextAttributes = TextAttributes.attribute
+        textField.text = text
+        textField.textAlignment = .center
+        textField.adjustsFontSizeToFitWidth = true
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         //Disable camera for Simulators
@@ -130,23 +127,21 @@ class MemeViewController: UIViewController {
     }
     
     @IBAction func albumAction(_ sender: UIBarButtonItem) {
-        let photoPickerVC = UIImagePickerController()
-        photoPickerVC.delegate = self
-        photoPickerVC.sourceType = .photoLibrary
-        photoPickerVC.allowsEditing = true
-        photoPickerVC.modalPresentationStyle = .fullScreen
-        self.present(photoPickerVC, animated: true, completion: nil)
+        self.present(self.chooseImageFromCameraOrPhoto(.photoLibrary), animated: true, completion: nil)
     }
     
     @IBAction func cameraAction(_ sender: UIBarButtonItem) {
-        let photoPickerVC = UIImagePickerController()
-        photoPickerVC.delegate = self
-        photoPickerVC.modalPresentationStyle = .fullScreen
-        photoPickerVC.sourceType = .camera
-        photoPickerVC.allowsEditing = true
-        self.present(photoPickerVC, animated: true, completion: nil)
+        self.present(self.chooseImageFromCameraOrPhoto(.camera), animated: true, completion: nil)
     }
     
+    private func chooseImageFromCameraOrPhoto(_ source: UIImagePickerController.SourceType) -> UIImagePickerController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = source
+        imagePicker.allowsEditing = true
+        imagePicker.modalPresentationStyle = .fullScreen
+        return imagePicker
+    }
     
     
 }
